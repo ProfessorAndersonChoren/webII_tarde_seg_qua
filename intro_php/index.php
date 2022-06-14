5dcbcab5f2dddfe7a1652917db8296c67fcbfe1f
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-06-14 14:10:53 -0300

    Intro PHP

---
+++ intro_php/index.php
@@ -0,0 +1,12 @@
+<?php
+//  Como imprimir algo na tela
+echo '<h1>Hello World!!!</h1>';
+
+$name = 'Thiago';
+echo "<h1>Bem-vindo ao meu sistema $name!!!</h1>";
+
+// Tipos de dados
+$integer = 100;
+$float = 100.89;
+$string = 'Um texto qualquer com o número 10';
+$boolean = true; // false
