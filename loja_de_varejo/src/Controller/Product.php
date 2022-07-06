e377f4e36751fc8bdb0effc3e4b6ed186ff420c5
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-07-06 15:29:00 -0300

    Adição das validações ao controlador

--- loja_de_varejo/src/Controller/Product.php
+++ loja_de_varejo/src/Controller/Product.php
@@ -2,6 +2,8 @@
 
 namespace APP\Controller;
 
+use APP\Model\Product;
+use APP\Model\Validation;
 use APP\Utils\Redirect;
 
 require_once '../../vendor/autoload.php';
@@ -14,5 +16,38 @@ if (empty($_POST)) {
 }
 
 $productName = $_POST["name"];
-$productPrice = $_POST["price"];
-$productQuantity = $_POST["quantity"];
+$productPrice = (float) $_POST["price"];
+$productQuantity = (float) $_POST["quantity"];
+
+$error = array();
+
+if(!Validation::validateName($productName)){
+    array_push($error, "O nome do produto deve conter ao menos 5 caracteres!!!");
+}
+
+if(!Validation::validateNumber($productPrice)){
+    array_push($error, "O preço do produto deve ser maior que zero!!!");
+}
+
+if(!Validation::validateNumber($productQuantity)){
+    array_push($error, "A quantidade do produto deve ser maior que zero!!!");
+}
+
+if($error){ // Se existirem erros
+    Redirect::redirect(
+        message: $error,
+        type: 'warning'
+    );
+}else{
+    $product = new Product(
+        name: $productName,
+        quantity: $productQuantity,
+        price: $productPrice
+    );
+    
+    // TODO Salvar no banco de dados
+
+    Redirect::redirect(
+        message: "Produto cadastrado com sucesso!!!"
+    );
+}
